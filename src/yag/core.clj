1b98ddc7b4b96d3807c1025058951ca0c1efec61
Stanislaw Karkavin <me@xdefrag.dev>
2019-09-23 00:10:57 +0300

    Add green square with moves

--- src/yag/core.clj
+++ src/yag/core.clj
@@ -24,14 +24,6 @@
    :y (q/random height)
    :color 255})
 
-(defn setup
-  []
-  (q/frame-rate 60)
-  (q/color-mode :rgb)
-  {:stars (take 100 (repeatedly random-star))
-   :ship {:x (/ width 2)
-          :y (/ height 2)}})
-
 (defn update-starfield
   [state]
   (assoc state :stars 
@@ -48,18 +40,79 @@
   (q/background 0)
   (doseq [star (:stars state)]
     (draw-star star))
-  (into state))
+  state)
 
+(defn update-ship
+  [state]
+  state)
+
+(defn draw-ship
+  [state]
+  (let [ship (:ship state)]
+    (q/no-fill)
+    (q/stroke-weight (q/random 1 2))
+    (q/stroke 0 255 0)
+    (q/rect (:x ship) (:y ship) 16 16))
+  state)
+
+;FIXME: really buggy impl.
+(defn move-ship
+  [direction coord]
+  (let [x (:x coord)]
+    (if (and (<= x (- width 16)) (>= x 16))
+      (assoc coord :x (if (= direction :left) (- x 16) (+ x 16)))
+      (assoc coord :x (if (= direction :left) (+ x 16) (- x 16))))))
+      
+(defn left-ship
+  [coord]
+  (move-ship :left coord))
+
+(defn right-ship
+  [coord]
+  (move-ship :right coord))
+
+(defn draw-debug
+  [state]
+  (q/fill 255)
+  (let [font (:debug (:font state))
+        ship (:ship state)]
+    (q/text-font font)
+    (q/text (str "ship:" (:x ship) " " (:y ship)) 20 50))
+  state)
+
+(defn on-key-down
+  [state event]
+  (case (:key event)
+    (:h :left) (update-in state [:ship] left-ship)
+    (:l :right) (update-in state [:ship] right-ship)
+    state))
+
+(defn on-key-up
+  [state event]
+  state)
+
+(defn setup
+  []
+  (q/frame-rate 60)
+  (q/color-mode :rgb)
+  {:font { :debug (q/create-font "Arial" 16)}
+   :stars (take 100 (repeatedly random-star))
+   :ship {:x (- (/ width 2) 16)
+          :y (-  height (/ height 6))
+          :color 255}})
 
 (defn update-state
   [state]
   (->> state
-     update-starfield))
+     update-starfield
+     update-ship))
 
 (defn draw-state
   [state]
   (->> state
-     draw-starfield))
+     draw-starfield
+     draw-ship
+     draw-debug))
 
 (declare yag)
 
@@ -69,5 +122,7 @@
   :setup setup
   :update update-state
   :draw draw-state
+  :key-pressed on-key-down
+  :key-released on-key-up
   :features [:keep-on-top]
   :middleware [m/fun-mode])
