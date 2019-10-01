04738bde18bfea0f0ba0138957e2baab74e4a78a
Stanislaw Karkavin <me@xdefrag.dev>
2019-10-01 21:34:15 +0300

    Add deadly laser

--- src/yag/core.clj
+++ src/yag/core.clj
@@ -26,7 +26,7 @@
 
 (defn update-starfield
   [state]
-  (assoc state :stars 
+  (assoc state :stars
     (map inc-coord (:stars state))))
 
 (defn draw-star
@@ -48,21 +48,25 @@
 
 (defn draw-ship
   [state]
-  (let [ship (:ship state)]
+  (let [ship (:ship state)
+        coord (:coord ship)]
     (q/no-fill)
     (q/stroke-weight (q/random 1 2))
     (q/stroke 0 255 0)
-    (q/rect (:x ship) (:y ship) 16 16))
+    (q/rect (:x coord) (:y coord) 16 16))
   state)
 
-;FIXME: really buggy impl.
 (defn move-ship
   [direction coord]
-  (let [x (:x coord)]
-    (if (and (<= x (- width 16)) (>= x 16))
-      (assoc coord :x (if (= direction :left) (- x 16) (+ x 16)))
-      (assoc coord :x (if (= direction :left) (+ x 16) (- x 16))))))
-      
+  (let [x (:x coord)
+        y (:y coord)
+        new-x (cond
+                (> x width) (- width 16)
+                (< x 0) (+ 0 16)
+                (= direction :left) (- x 16)
+                (= direction :right) (+ x 16))]
+    {:x new-x :y y}))
+
 (defn left-ship
   [coord]
   (move-ship :left coord))
@@ -71,47 +75,71 @@
   [coord]
   (move-ship :right coord))
 
+(defn update-bullet
+  [state]
+  state)
+
+(defn draw-bullet
+  [state]
+  (let [bullets (:bullets state)
+        coord (:coord (:ship state))
+        ship-x (:x coord)
+        ship-y (:y coord)
+        ship-x-c (+ ship-x 8)]
+    (when bullets ((q/stroke 255 0 0)
+                   (q/line [ship-x-c 0] [ship-x-c ship-y]))))
+  state)
+
 (defn draw-debug
   [state]
   (q/fill 255)
   (let [font (:debug (:font state))
-        ship (:ship state)]
+        bullets (:bullets state)
+        ship (:ship state)
+        ship-coord (:coord ship)]
     (q/text-font font)
-    (q/text (str "ship:" (:x ship) " " (:y ship)) 20 50))
+    (q/text (str "ship:" (:x ship-coord) " " (:y ship-coord)) 20 50)
+    (q/text (str "bullets:" (if bullets "true" "false")) 20 80))
   state)
 
 (defn on-key-down
   [state event]
   (case (:key event)
-    (:h :left) (update-in state [:ship] left-ship)
-    (:l :right) (update-in state [:ship] right-ship)
+    (:h :left) (update-in state [:ship :coord] left-ship)
+    (:l :right) (update-in state [:ship :coord] right-ship)
+    (:space) (assoc-in state [:bullets] true)
     state))
 
 (defn on-key-up
   [state event]
-  state)
+  (case (:key event)
+    (:space)(assoc-in state [:bullets] false)
+    state))
 
 (defn setup
   []
   (q/frame-rate 60)
   (q/color-mode :rgb)
-  {:font { :debug (q/create-font "Arial" 16)}
+  {:font {:debug (q/create-font "Arial" 16)}
    :stars (take 100 (repeatedly random-star))
-   :ship {:x (- (/ width 2) 16)
-          :y (-  height (/ height 6))
+   :bullets false
+   :ship {:coord {:x (- (/ width 2) 16)
+                  :y (-  height (/ height 6))}
           :color 255}})
 
 (defn update-state
   [state]
   (->> state
      update-starfield
-     update-ship))
+     update-ship
+     update-bullet))
 
 (defn draw-state
   [state]
   (->> state
      draw-starfield
      draw-ship
+     draw-bullet
      draw-debug))
 
 (declare yag)
@@ -125,4 +153,4 @@
   :key-pressed on-key-down
   :key-released on-key-up
   :features [:keep-on-top]
-  :middleware [m/fun-mode])
+  :middleware [m/fun-mode m/pause-on-error])
